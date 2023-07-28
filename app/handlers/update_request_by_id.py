a510b54c139e36a5bcd117e1078f554ff8a4c605
Mark Achiles <markachilesflores2004@gmail.com>
2023-07-28 13:17:01 +0800

    Finished app/handlers/update_request_by_id.py

--- app/handlers/update_request_by_id.py
+++ app/handlers/update_request_by_id.py
@@ -1,16 +1,29 @@
 # src/handlers/updateRequestById.py
 
 import json
+import boto3
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("photo-booth-app")
 
 def handler(event, context):
     # Extract request ID from path parameter
     request_id = event['pathParameters']['requestId']
-
-    # Parse request body
-    request_body = json.loads(event['body'])
+    status = event["pathParameters"]["status"]
 
     # Update request in DynamoDB based on request ID
-    # ...
+    table.update_item(
+        Key={
+            'requestId': request_id 
+        },
+        UpdateExpression='SET #status_attr = :status_val',
+        ExpressionAttributeNames={
+            '#status_attr': 'status'
+        },
+        ExpressionAttributeValues={
+            ':status_val': status
+        }
+    )
 
     # Return a response
     response = {
